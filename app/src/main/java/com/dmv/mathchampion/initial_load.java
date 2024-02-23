4ece9a1bbbd645fa36d5c0699e9a88d4013b5653
David Mojica <davidmojicav@gmail.com>
2024-02-23 15:21:26 -0500

    Setting up animation

--- app/src/main/java/com/dmv/mathchampion/initial_load.java
+++ app/src/main/java/com/dmv/mathchampion/initial_load.java
@@ -1,6 +1,8 @@
 package com.dmv.mathchampion;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 public class initial_load extends AppCompatActivity {
@@ -9,5 +11,12 @@ public class initial_load extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.initial_load);
 
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(initial_load.this, MainActivity.class));
+                finish();
+            }
+        }, 2500);
     }
 }
