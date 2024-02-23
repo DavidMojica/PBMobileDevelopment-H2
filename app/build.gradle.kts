21f7346ee9f09ce8524bd3f0426bb424e8d80fc3
David Mojica <davidmojicav@gmail.com>
2024-02-23 15:08:51 -0500

    initial load resx created

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
 }
 
 dependencies {
-
+    implementation ("com.airbnb.android:lottie:6.3.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
---
+++ app/src/main/java/com/dmv/mathchampion/initial_load.java
@@ -0,0 +1,13 @@
+package com.dmv.mathchampion;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+public class initial_load extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.initial_load);
+
+    }
+}
